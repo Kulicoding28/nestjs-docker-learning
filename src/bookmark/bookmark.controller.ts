a38db7463082a4a217758c91d67aace1cf3410cc
Kulicoding28 <ahmadabdulaziz380@gmail.com>
2024-01-15 10:34:34 +0700

    learning-nest

---
+++ src/bookmark/bookmark.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
+import { BookmarkService } from './bookmark.service';
+import { GetUser } from 'src/auth/decorator';
+
+@UseGuards(JwtGuard)
+@Controller('bookmark')
+export class BookmarkController {
+  constructor(private bookmarkService: BookmarkService) {}
+
+  @Get()
+  getBookmarks(@GetUser('id') userId: number) {
+    return this.bookmarkService.getBookmarks(userId);
+  }
+
+  @Get(':id')
+  getBookmarksById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.getBookmarkById(userId, bookmarkId);
+  }
+}
---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class BookmarkService {
+  constructor(private prisma: PrismaService) {}
+
+  getBookmarks(userId: number) {
+    return this.prisma.bookmark.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  getBookmarkById(userId: number, bookmarkId: number) {
+    return this.prisma.bookmark.findFirst({
+      where: {
+        id: bookmarkId,
+        userId,
+      },
+    });
+  }
+}
